c6d9d62d9e8f5b4d3003ed26727ec8581e4c33aa
Thomas Schoffelen <thomas@schof.co>
2025-12-01 10:16:00 +0000

    Initial Commit

---
+++ Add to Obsidian/Add_to_ObsidianApp.swift
@@ -0,0 +1,17 @@
+//
+//  Add_to_ObsidianApp.swift
+//  Add to Obsidian
+//
+//  Created by Thomas Schoffelen on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Add_to_ObsidianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Add to Obsidian/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Add to Obsidian
+//
+//  Created by Thomas Schoffelen on 01/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
